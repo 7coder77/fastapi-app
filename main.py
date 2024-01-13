3acb094b112b00c60074a350fee9388422a83a2e
Ani <aniruddhajoshi12@gmail.com>
2024-01-13 14:20:51 +0530

    download added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from typing import Optional
 
-from fastapi import FastAPI
+import uvicorn
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from starlette.responses import StreamingResponse
+import shutil
+import os
 
 app = FastAPI()
 
@@ -12,3 +18,16 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/downloadfile/{filename}")
+async def download_file(filename: str):
+    # Check if the file exists
+    if not os.path.exists(filename):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    # Open the file and return it as a StreamingResponse
+    file_path = os.path.abspath(filename)
+    return StreamingResponse(open(file_path, "rb"), media_type="application/pdf", headers={"Content-Disposition": f"attachment; filename={filename}"})
+
+if __name__=="__main__":
+    uvicorn.run(app)
