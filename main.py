9a2771f190158a2b530a55c0408973627b4fad7e
Ani <aniruddhajoshi12@gmail.com>
2024-03-09 13:33:52 +0530

    changes to my root

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Aniruddha"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
